a7d0379f86e3242fd100f2389152bf98a6ffb5aa
marcel.corso@gmail.com <marcel.corso@gmail.com>
2013-02-15 16:06:20 +0100

    organize things

--- HelloUniverse/ViewController.m
+++ HelloUniverse/ViewController.m
@@ -36,8 +36,8 @@
     return [SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter];
 }
 
-- (void)fetchTimelineForUser:(NSString *)username
-{
+- (void)fetchFollowing {
+    
     NSLog(@"fetchTimelineForUser");
     //  Step 0: Check that the user has local Twitter accounts
     if ([self userHasAccessToTwitter]) {
@@ -56,14 +56,14 @@
              if (granted) {
                  NSLog(@"userHasAccessToTwitter.granted");
                  //  Step 2:  Create a request
-                 NSArray *twitterAccounts =
-                 [self.accountStore accountsWithAccountType:twitterAccountType];
-                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com"
-                               @"/1.1/statuses/user_timeline.json"];
-                 NSDictionary *params = @{@"screen_name" : username,
-                                          @"include_rts" : @"0",
-                                          @"trim_user" : @"1",
-                                          @"count" : @"1"};
+                 NSArray *twitterAccounts = [self.accountStore accountsWithAccountType:twitterAccountType];
+                 // TODO allow the user to select wich account he wants to use
+                 ACAccount *account = [twitterAccounts lastObject];
+                 NSString *screenName = [account username];
+                 
+                 NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/friends/ids.json"];
+                 NSDictionary *params = @{@"screen_name" : screenName,
+                                          @"count" : @"50000"};
                  SLRequest *request =
                  [SLRequest requestForServiceType:SLServiceTypeTwitter
                                     requestMethod:SLRequestMethodGET
@@ -71,7 +71,7 @@
                                        parameters:params];
                  
                  //  Attach an account to the request
-                 [request setAccount:[twitterAccounts lastObject]];
+                 [request setAccount: account];
                  
                  //  Step 3:  Execute the request
                  [request performRequestWithHandler:^(NSData *responseData,
@@ -83,13 +83,13 @@
                          if (urlResponse.statusCode >= 200 && urlResponse.statusCode < 300) {
                              NSLog(@"userHasAccessToTwitter.nice status code");
                              NSError *jsonError;
-                             NSDictionary *timelineData =
+                             NSDictionary *followingData =
                              [NSJSONSerialization
                               JSONObjectWithData:responseData
                               options:NSJSONReadingAllowFragments error:&jsonError];
                              
-                             if (timelineData) {
-                                 NSLog(@"Timeline Response: %@\n", timelineData);
+                             if (followingData) {
+                                 NSLog(@"Timeline Response: %@\n", followingData);
                              }
                              else {
                                  // Our JSON deserialization went awry
@@ -111,8 +111,115 @@
     } else {
         NSLog(@"no access to twitter");
     }
+    
 }
 
+- (ACAccount *)resolveAccount
+{
+    __block ACAccount *account = nil;
+    NSLog(@"fetchTimelineForUser");
+    //  Step 0: Check that the user has local Twitter accounts
+    if ([self userHasAccessToTwitter]) {
+        NSLog(@"userHasAccessToTwitter");
+        
+        //  Step 1:  Obtain access to the user's Twitter accounts
+        ACAccountType *twitterAccountType = [self.accountStore
+                                             accountTypeWithAccountTypeIdentifier:
+                                             ACAccountTypeIdentifierTwitter];
+        NSLog(@"userHasAccessToTwitter wazzup???");
+        [self.accountStore
+         requestAccessToAccountsWithType:twitterAccountType
+         options:NULL
+         completion:^(BOOL granted, NSError *error) {
+             NSLog(@"userHasAccessToTwitter.completion");
+             if (granted) {
+                 NSLog(@"userHasAccessToTwitter.granted");
+                 //  Step 2:  Create a request
+                 NSArray *twitterAccounts = [self.accountStore accountsWithAccountType:twitterAccountType];
+                 
+                 
+                 // TODO allow the user to select wich account he wants to use
+                 account = [twitterAccounts lastObject];
+             }
+             else {
+                 // Access was not granted, or an error occurred
+                 NSLog(@"%@", [error localizedDescription]);
+             }
+         }];
+    } else {
+        NSLog(@"no access to twitter");
+    }
+    
+    return account;
+}
+
+- (NSArray *)fetchFriends:(ACAccount *)account
+{
+    __block NSArray *friendIds = [NSArray array];
+    NSString *screenName = [account username];
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/friends/ids.json"];
+    NSDictionary *params = @{@"screen_name" : screenName,
+                             @"count" : @"50000"};
+    SLRequest *request =
+    [SLRequest requestForServiceType:SLServiceTypeTwitter
+                       requestMethod:SLRequestMethodGET
+                                 URL:url
+                          parameters:params];
+    
+    //  Attach an account to the request
+    [request setAccount: account];
+    
+    //  Step 3:  Execute the request
+    [request performRequestWithHandler:^(NSData *responseData,
+                                         NSHTTPURLResponse *urlResponse,
+                                         NSError *error) {
+        NSLog(@"userHasAccessToTwitter.after_execute");
+        if (responseData) {
+            NSLog(@"userHasAccessToTwitter.has_response_data");
+            if (urlResponse.statusCode >= 200 && urlResponse.statusCode < 300) {
+                NSLog(@"userHasAccessToTwitter.nice status code");
+                NSError *jsonError;
+                NSDictionary *friendsData =
+                [NSJSONSerialization
+                 JSONObjectWithData:responseData
+                 options:NSJSONReadingAllowFragments error:&jsonError];
+                
+                if (friendsData) {
+                    NSLog(@"Timeline Response: %@\n", friendsData);
+                    friendIds = [friendsData valueForKey:@"friends"];
+                }
+                else {
+                    // Our JSON deserialization went awry
+                    NSLog(@"JSON Error: %@", [jsonError localizedDescription]);
+                }
+            }
+            else {
+                // The server did not respond successfully... were we rate-limited?
+                NSLog(@"The response status code is %d", urlResponse.statusCode);
+            }
+        }
+    }];
+
+    return friendIds;
+}
+
+- (NSArray *) fetchUsers:(NSArray *)friendIds
+{
+    NSArray *matchedUsers = [NSArray array];
+    // request http://pre.shuffler.fm/users.json?twitter_id=4,5,6,7
+    
+}
+
+
+- (void)matchFriends
+{
+    // find out what twitter account to get the friends from
+    ACAccount *account = [self resolveAccount];
+    // request N twitter friends
+    NSArray *friendIds = [self fetchFriends:account];
+    // request the shuffler users with those twitter_ids
+    NSArray *matchedUsers = [self fetchUsers:friendIds];
+}
 
 - (IBAction)changeGreeting:(id)sender {
     self.userID = self.textField.text;
@@ -125,7 +232,10 @@
     self.label.text = greeting;
     
     
-    [self fetchTimelineForUser:idString];
+    // [self fetchTimelineForUser:idString];
+    
+    
+    [self matchFriends];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
